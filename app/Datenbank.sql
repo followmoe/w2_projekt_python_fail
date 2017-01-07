b791ee00c231b229a3770f9458cd5af87d6d7a93
ElBibao <t.biba@gmx.de>
2017-01-07 16:40:53 +0100

    have to commit

--- app/Datenbank.sql
+++ app/Datenbank.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.1.1 on Sa Jan 7 15:05:34 2017
+-- File generated with SQLiteStudio v3.1.1 on Sa Jan 7 16:39:06 2017
 --
 -- Text encoding used: System
 --
@@ -15,29 +15,44 @@ CREATE TABLE Ausstattung (
 );
 
 INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (1, 'WLAN');
-INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (2, 'Schwimmbad/Sauna');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (2, 'Schwimmbad');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (3, 'Sauna');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (4, 'TV');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (5, 'Sky/Entertainment');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (6, 'Minibar');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (7, 'Bar/Lobby');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (8, 'Parkplatz');
+INSERT INTO Ausstattung (Ausstattungs_ID, Name) VALUES (9, 'Haustiere');
 
 -- Table: Hotel
 DROP TABLE IF EXISTS Hotel;
 
 CREATE TABLE Hotel (
-    Hotel__id     INTEGER      PRIMARY KEY AUTOINCREMENT,
-    Name          VARCHAR (70) NOT NULL,
-    Sterne        INT (6),
-    Zimmer_Anzahl INT          NOT NULL,
-    Zimmer_ID     INTEGER      NOT NULL
-                               REFERENCES zimmer (Zimmer_ID) 
+    Hotel__id             INTEGER      PRIMARY KEY AUTOINCREMENT,
+    Name                  VARCHAR (70) NOT NULL,
+    Sterne                INT (6),
+    Zimmer_Anzahl         INT          NOT NULL,
+    Hotel_Ausstattungs_ID INTEGER      NOT NULL
+                                       REFERENCES Hotel_Ausstattung (Hotel_Ausstattung_ID) ON UPDATE CASCADE
 );
 
+INSERT INTO Hotel (Hotel__id, Name, Sterne, Zimmer_Anzahl, Hotel_Ausstattungs_ID) VALUES (1, 'Asgard Hotel', 3, 63, 1);
 
 -- Table: Hotel_Ausstattung
 DROP TABLE IF EXISTS Hotel_Ausstattung;
 
 CREATE TABLE Hotel_Ausstattung (
-    Hotel_id       INTEGER REFERENCES Hotel (Hotel__id),
-    Ausstattung_ID INTEGER REFERENCES Ausstattung (Ausstattungs_ID) 
+    Hotel_id             INTEGER REFERENCES Hotel (Hotel__id),
+    Ausstattung_ID       INTEGER REFERENCES Ausstattung (Ausstattungs_ID),
+    Hotel_Ausstattung_ID INTEGER NOT NULL
 );
 
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 1, 1);
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 8, 1);
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 9, 1);
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 6, 1);
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 3, 1);
+INSERT INTO Hotel_Ausstattung (Hotel_id, Ausstattung_ID, Hotel_Ausstattung_ID) VALUES (1, 4, 1);
 
 -- Table: Kategorie
 DROP TABLE IF EXISTS Kategorie;
@@ -76,6 +91,11 @@ CREATE TABLE zimmer (
 );
 
 INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (1, 1, 2, 1);
+INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (2, 2, 2, 2);
+INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (3, 3, 1, 3);
+INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (4, 4, 0, 6);
+INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (5, 5, 2, 4);
+INSERT INTO zimmer (Zimmer_ID, Zimmer_Nummer, BettenZahl, Kategorie_ID) VALUES (6, 6, 2, 5);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
