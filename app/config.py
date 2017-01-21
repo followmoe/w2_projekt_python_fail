82f33985faa639b43a46c6154611fd9d665c24b6
Moritz Mueller <moritz.mueller@posteo.de>
2017-01-21 12:40:05 +0100

    Hinzufügen einer Database connection passwort hash und config datei. Erstes Ausplitten der Dateien.

---
+++ app/config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///Datenbank.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+#erstellt mittels urandom(24)
+SECRET_KEY = 'O\xb9;\x19\x0e\x8c\x1c\x8d\xe47\xb1o\xd6\x96Wx\xb6\x06#J6F\xfb\xea'
+DEBUG = True
